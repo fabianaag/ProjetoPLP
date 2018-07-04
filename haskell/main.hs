fea05e35aa7e1843f01a470875d1b7b98217023b
Thales Souto <thaleshenriquepb@gmail.com>
2018-07-04 10:37:27 -0300

    boa

---
+++ haskell/main.hs
@@ -0,0 +1,36 @@
+import System.IO
+import Data.Char
+
+cabecalho :: IO ()
+cabecalho = do
+ putStrLn (". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .")
+ putStrLn (". ______________________________ S H O W ______________________________ .")
+ putStrLn (". ________________________________ D O ________________________________ .")
+ putStrLn (". ____________________________ M I L H A O ____________________________ .")
+ putStrLn (". _____________________________________________________________________ .")
+ putStrLn (".  Teste seus conhecimentos e concorra ao premio maximo de R$ 1 milhao  .")
+ putStrLn (". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .")
+
+
+menuCategorias :: IO ()
+menuCategorias = do
+  putStrLn "Qual categoria de questoes voce deseja?"
+  putStrLn "1 - Computacao"
+  putStrLn "2 - Conhecimentos Gerais"
+
+
+lerQuestoes_cc :: IO ()
+lerQuestoes_cc =
+    handle <- openFile "questoes_cc.txt" ReadMode
+    contents <- hGetContents handle
+    hClose handle
+
+
+main :: IO ()
+main = do
+ cabecalho
+ putStrLn "Diga-me o seu nome: "
+ nome <- getLine
+ menuCategorias
+ categoria <- getLine
+ lerQuestoes_cc
