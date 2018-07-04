d2333d7ba552368cd202c336ba21be86192f58c1
Thales Souto <thaleshenriquepb@gmail.com>
2018-07-04 09:52:47 -0300

    txt das questoes

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,3 +1,6 @@
+import System.IO
+import Data.Char
+
 cabecalho :: IO ()
 cabecalho = do
  putStrLn (". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .")
@@ -8,6 +11,28 @@ cabecalho = do
  putStrLn (".  Teste seus conhecimentos e concorra ao premio maximo de R$ 1 milhao  .")
  putStrLn (". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .")
 
+
+menuCategorias :: IO ()
+menuCategorias = do
+  putStrLn "Qual categoria de questoes voce deseja?"
+  putStrLn "1 - Computacao"
+  putStrLn "2 - Conhecimentos Gerais"
+
+
+lerQuestoes_cc :: IO ()
+lerQuestoes_cc = do
+  handle <- openFile "questoes_cc.txt" ReadMode
+  contents <- hGetContents handle
+  let questao_linha_linha = lines contents
+  putStr $ unlines questao_linha_linha
+  hClose handle
+
+
 main :: IO ()
 main = do
  cabecalho
+ putStrLn "Diga-me o seu nome: "
+ nome <- getLine
+ menuCategorias
+ categoria <- getLine
+ lerQuestoes_cc
