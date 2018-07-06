8183883293457605b5959a3977bdc633e769c8bf
evelinnec <evelinne.araujo@ccc.ufcg.edu.br>
2018-07-05 23:45:22 -0300

    Alterei matrizes das questões e fazendo o básico do jogo

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,5 +1,32 @@
 import System.IO
 import Data.Char
+import System.Random (randomRIO)
+
+geraPseudoRandom val = randomRIO (1,val)
+
+{- This code is from Data.List.Split module -}
+splitOn :: (a -> Bool) -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn f l@(x:xs)
+  | f x = splitOn f xs
+  | otherwise = let (h,t) = break f l in h:(splitOn f t)
+
+{- this function remove \n from each string in list-}
+removeN :: [[Char]] -> [[Char]]
+removeN list
+  | list == [] = list
+  | otherwise = (tail (head list)) : (removeN (tail list))
+
+{- this method take every single part of a question and answers and put in a single ==> matriz of each question -}
+converterListaParaMatriz :: [[Char]] -> [[[Char]]]
+converterListaParaMatriz (p:a:b:c:d:r:list)
+  | list /= [] = ([p]++[a]++[b]++[c]++[d]++[r]):(converterListaParaMatriz list)
+  | otherwise = ([p]++[a]++[b]++[c]++[d]++[r]):[]
+
+randomizaMatriz :: [[[Char]]] -> [[Char]]-> [[[Char]]]
+randomizaMatriz list newHead = newHead : [x | x <- list, x /= newHead]
+
+
 
 cabecalho :: IO ()
 cabecalho = do
@@ -19,20 +46,33 @@ menuCategorias = do
   putStrLn "2 - Conhecimentos Gerais"
 
 
-lerQuestoes :: String -> IO ()
+lerQuestoes :: String -> IO ([[Char]])
 lerQuestoes categoria = do
     if(categoria == "1") then do
       handle <- openFile "questoes_cc.txt" ReadMode
       contents <- hGetContents handle
-      putStrLn contents
-      hClose handle
+      let questoes = (splitOn (==';') contents)
+      return ((head questoes) : (removeN (tail questoes)))
     else do
-      handle <- openFile "questoes_ge.txt" ReadMode
+      handle <- openFile "questoes_cc.txt" ReadMode
       contents <- hGetContents handle
-      putStrLn contents
-      hClose handle
+      let questoes = (splitOn (==';') contents)
+      return ((head questoes) : (removeN (tail questoes))) {- [[CHAR]]-}
 
 
+joga :: [[[Char]]] -> Int -> IO ()
+joga [] _ = print "Tu ganho mizeravi"
+joga questoes premio =  do
+  putStrLn (( head questoes )!!0)
+  putStrLn (( head questoes )!!1)
+  putStrLn (( head questoes )!!2)
+  putStrLn (( head questoes )!!3)
+  putStrLn (( head questoes )!!4)
+  input <- getLine
+  if (( head questoes )!!5 == input) then do
+    joga (tail questoes) (premio+100)
+  else do
+    print "Erro Mizeravi"
 main :: IO ()
 main = do
  cabecalho
@@ -40,4 +80,7 @@ main = do
  nome <- getLine
  menuCategorias
  categoria <- getLine
- lerQuestoes categoria
+ questoes <- lerQuestoes categoria
+ rand <- geraPseudoRandom (length $ converterListaParaMatriz questoes)
+ print rand
+ joga (last [randomizaMatriz (converterListaParaMatriz questoes) (converterListaParaMatriz questoes !! (rand-1)) | x <- [1..rand]]) 0{- Matriz randomizada uma unica vez-}
