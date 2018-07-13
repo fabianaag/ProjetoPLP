61ee5073320eb99bad440683c8a021d47bf3e0a0
113210810 THALES HENRIQUE DANTAS SOUTO <thaleshds@lcc2-34.lcc.ufcg.edu.br>
2018-07-13 11:32:58 -0300

    cabecalho, nome e opcao

---
+++ prolog/teste.pl
@@ -0,0 +1,23 @@
+%! Define main como a regra principal do programa
+:- initialization(main).
+
+cabecalho:-
+	write(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."),nl,
+	write(". ______________________________ S H O W ______________________________ ."),nl,
+	write(". ________________________________ D O ________________________________ ."),nl,
+	write(". ____________________________ M I L H A O ____________________________ ."),nl,
+  write(". _____________________________________________________________________ ."),nl,
+  write(".  Teste seus conhecimentos e concorra ao premio maximo de R$ 1 milhao  ."),nl,
+  write(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."),nl.
+
+
+
+main:-
+  cabecalho,
+  write("Bota teu nome aí arrombado: "),
+  read_line_to_string(user_input, Nome),
+  write(Nome), writeln(", qual categoria de questoes voce deseja?"),
+  writeln(" 1 - Computacao"),
+  writeln(" 2 - Conhecimentos Gerais"),
+  read_line_to_string(user_input, Opcao),
+  write("Voce escolheu "), write(Opcao), writeln(", boa sorte!").
