506c74ce8cdc0e4158c1b9885bf78a949fc16f87
Fabiana Alves <fabianaag@users.noreply.github.com>
2018-07-27 14:17:16 -0300

    Atualização

--- prolog/main_com_loop.pl
+++ prolog/main_com_loop.pl
@@ -4,7 +4,6 @@ questao_facil_ge("Quanto vale 2+2?\nA) 5\nB) 7\nC) 4\nD) 2","c").
 questao_facil_ge("O que esta escrito na bandeira do Brasil?\nA) Ordem e Regresso\nB) Ordem e Retrocesso\nC) Ordem e Progresso\nD) Progresso e Ordem","c").
 questao_facil_ge("Qual a traducao da palavra inglesa 'horse'?\nA) Casa\nB) Cavalo\nC) Elefante\nD) Hipopotamo","b").
 
-
 lista_perguntas_facil_ge(ListaFacilGe):-
           findall(Pfg, questao_facil_ge(Pfg, _), ListaFacilGe).
 
@@ -20,22 +19,10 @@ itera_sobre_perguntas([H|T]):-
                        continua(Resultado),
                        itera_sobre_perguntas(T).
 
-
-
 verifica_resposta(Resposta, AlternCorreta, Resultado):-
                   Resposta =:= AlternCorreta -> Resultado = 1; Resultado = 0.
 
-
 continua(R):- R =:= 0 -> writeln("Seu premio foi tanto"), halt(0) ; writeln("").
-
-imprime_resultado(R):- R =:= 1 -> writeln("Voce acertou"); writeln("Voce errou!").
-
-
-escolha_categoria("1", "Computacao").
-escolha_categoria("2", "Conhecimentos Gerais").
-
-categoria_cc():- write("1 - Computacao"), nl.
-categoria_ge():- write("2 - Conhecimentos Gerais"), nl.
 
 add_elem_inicio(X, L, [X|L]).
 add_elem_fim(T, [H], L):- add_elem_inicio(H, [T], L).
@@ -59,9 +46,11 @@ escolher_perguntas(List, List3) :-
     nth0(Index2, List2, Elem2),
     
     add_elem_fim(Elem2, List1, List3).
-    %rem_elem(Elem2, List2, List4).
 
-cabecalho:-
+categoria("1", "Computacao").
+categoria("2", "Conhecimentos Gerais").
+
+intro:-
      write(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."), nl,
      write(". ______________________________ S H O W ______________________________ ."), nl,
      write(". ________________________________ D O ________________________________ ."), nl,
@@ -70,20 +59,53 @@ cabecalho:-
      write(".  Teste seus conhecimentos e concorra ao premio maximo de R$ 1 milhao  ."), nl,
      write(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."), nl.
 
+leitura_nome(Nome) :-
+    write("Qual o seu nome? "),
+    read_line_to_string(user_input, Nome).
+
+leitura_categoria(Nome, Opcao) :-
+    write(Nome),
+    write(", qual categoria de questoes voce deseja?"), nl,
+    write("1 - Computacao"), nl,
+    write("2 - Conhecimentos Gerais"), nl,
+    read_line_to_string(user_input, Opcao).
+
+impressao_categoria(Opcao) :-
+    write("Voce escolheu "),
+    categoria(Opcao, TextoCategoria),
+    write(TextoCategoria),
+    write(", boa sorte!"), nl.
+
+impressao_pergunta(Opcao, Nivel, Indice) :-
+    questao(Opcao, Nivel, Indice, Pergunta, A, B, C, D, _),
+    write(Pergunta), nl,
+    write(A), nl,
+    write(B), nl,
+    write(C), nl,
+    write(D), nl.
+
+leitura_resposta(Resposta) :- read_line_to_string(user_input, Resposta).
+
+impressao_resposta(Resposta) :-
+    write("Voce escolheu: "),
+    write(Resposta), nl.
+
+impressao_alternativa_correta(Opcao, Nivel, Indice, AlternativaCorreta) :-
+    questao(Opcao, Nivel, Indice, _, _, _, _, _, AlternativaCorreta),
+    write("Correta: "),
+    write(AlternativaCorreta), nl.
+
+imprime_resultado(Resultado) :-
+    Resultado =:= 1 -> writeln("Voce acertou");
+    writeln("Voce errou!"), halt(0).
+
 main:-
-   cabecalho,
-   write("Qual o seu nome? "),
-   read_line_to_string(user_input, Nome),
+    intro,
+    leitura_nome(Nome),
+    leitura_categoria(Nome, Opcao),
+    impressao_categoria(Opcao),
 
-   write(Nome), writeln(", qual categoria de questoes voce deseja?"),
-   categoria_cc(),
-   categoria_ge(),
-   read_line_to_string(user_input, Opcao),
+    lista_perguntas_facil_ge(Fg),
+    escolher_perguntas(Fg, Fga),
+    itera_sobre_perguntas(Fga).
 
-   write("Voce escolheu "), escolha_categoria(Opcao, X), write(X), writeln(", boa sorte!"),
-
-
-
-   lista_perguntas_facil_ge(Fg),
-   escolher_perguntas(Fg, Fga),
-   itera_sobre_perguntas(Fga).
